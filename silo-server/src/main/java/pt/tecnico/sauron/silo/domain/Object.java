dfb173e35de92150a4b0c948c8fb8a9cbd3b142e
TiagoMAB <tiago.barroso1@hotmail.com>
2020-03-29 21:18:45 +0100

    Added identifier validation for type CAR and PERSON

--- silo-server/src/main/java/pt/tecnico/sauron/silo/domain/Object.java
+++ silo-server/src/main/java/pt/tecnico/sauron/silo/domain/Object.java
@@ -38,26 +38,36 @@ public class Object {
             throw new InvalidIdentifierException(identifier);
         }
 
-        if (type == Type.CAR) {
-            if (identifier.length() != 6) {
-                throw new InvalidIdentifierException(identifier);
-            }
+        if (type == Type.CAR && identifier.length() == 6) {
 
+            String[] patterns = {"^\\d{2}[A-Z]{2}\\d{2}", "^[A-Z]{2}\\d{4}", "^\\d{4}[A-Z]{2}", "^\\d{2}[A-Z]{4}", "^[A-Z]{4}\\d{2}", "^[A-Z]{2}\\d{2}[A-Z]{2}"};
+            for (String pattern: patterns) {
+                if (identifier.matches(pattern)) {
+                    this.identifier = identifier;
+                    return;
+                }
+            }
         }
 
         if (type == Type.PERSON) {
             Long n;
+
             try {
                 n = Long.parseUnsignedLong(identifier);
             }
             catch (NumberFormatException e) {
                 throw new InvalidIdentifierException(identifier);
             }
+
             if (n > 9223372036854775807L) {
                 throw new InvalidIdentifierException(identifier);
             }
+
             this.identifier = identifier;
+            return;
         }
+
+        throw new InvalidIdentifierException(identifier);
     }
 
 }
