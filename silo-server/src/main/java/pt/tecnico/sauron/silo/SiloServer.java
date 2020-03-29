526e772f48848cc8a442fe56ecb17354604d7ba3
Tiago Barroso <37189444+TiagoMAB@users.noreply.github.com>
2020-03-29 17:23:29 +0100

    Version 0.3.0
    * creation of SiloServer and domain package
    * Silo Server Skeleton

---
+++ silo-server/src/main/java/pt/tecnico/sauron/silo/SiloServer.java
@@ -0,0 +1,60 @@
+package pt.tecnico.sauron.silo;
+
+import io.grpc.stub.StreamObserver;
+import pt.tecnico.sauron.silo.domain.Silo;
+import pt.tecnico.sauron.silo.grpc.*;
+
+import java.util.logging.Logger;
+
+public class SiloServer extends SiloGrpc.SiloImplBase {
+
+    private static final Logger LOGGER = Logger.getLogger(SiloServer.class.getName());
+
+    private final Silo silo = new Silo();
+
+    @Override
+    public void camInfo(CamInfoRequest request, StreamObserver<CamInfoResponse> responseObserver) {
+        super.camInfo(request, responseObserver);
+    }
+
+    @Override
+    public void camJoin(CamJoinRequest request, StreamObserver<CamJoinResponse> responseObserver) {
+        super.camJoin(request, responseObserver);
+    }
+
+    @Override
+    public void report(ReportRequest request, StreamObserver<ReportResponse> responseObserver) {
+        super.report(request, responseObserver);
+    }
+
+    @Override
+    public void track(TrackRequest request, StreamObserver<TrackResponse> responseObserver) {
+        super.track(request, responseObserver);
+    }
+
+    @Override
+    public void trackMatch(TrackMatchRequest request, StreamObserver<TrackMatchResponse> responseObserver) {
+        super.trackMatch(request, responseObserver);
+    }
+
+    @Override
+    public void trace(TraceRequest request, StreamObserver<TraceResponse> responseObserver) {
+        super.trace(request, responseObserver);
+    }
+
+    @Override
+    public void ctrlPing(CtrlPingRequest request, StreamObserver<CtrlPingResponse> responseObserver) {
+        super.ctrlPing(request, responseObserver);
+    }
+
+    @Override
+    public void ctrlClear(CtrlClearRequest request, StreamObserver<CtrlClearResponse> responseObserver) {
+        super.ctrlClear(request, responseObserver);
+    }
+
+    @Override
+    public void ctrlInit(CtrlInitRequest request, StreamObserver<CtrlInitResponse> responseObserver) {
+        super.ctrlInit(request, responseObserver);
+    }
+
+}
