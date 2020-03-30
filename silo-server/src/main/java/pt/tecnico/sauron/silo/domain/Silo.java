1eff3ded6dcb413dae41cfc7ec535c4ea7cdecb5
TiagoMAB <tiago.barroso1@hotmail.com>
2020-03-30 18:23:42 +0100

    Removed test code

--- silo-server/src/main/java/pt/tecnico/sauron/silo/domain/Silo.java
+++ silo-server/src/main/java/pt/tecnico/sauron/silo/domain/Silo.java
@@ -13,49 +13,7 @@ public class Silo {
     private TreeSet observations = new TreeSet<Observation>();
     private HashSet cameras = new HashSet<Camera>();
 
-    public Silo() {
-        try {
-            Object o = new Object(Type.CAR, "AABB22");
-            objects.put(o.getIdentifier(), o);
-
-            Date date = new Date();
-            long time = date.getTime();
-            Timestamp ts = new Timestamp(time);
-            observations.add(new Observation(o, ts));
-
-            TimeUnit.SECONDS.sleep(2);
-
-            Object o2 = new Object(Type.CAR, "AABB22");
-            objects.put(o2.getIdentifier(), o2);
-
-            date = new Date();
-            time = date.getTime();
-            ts = new Timestamp(time);
-
-            observations.add(new Observation(o2, ts));
-
-            TimeUnit.SECONDS.sleep(3);
-
-            Object o3 = new Object(Type.CAR, "AABB22");
-            objects.put(o3.getIdentifier(), o3);
-
-            date = new Date();
-            time = date.getTime();
-            ts = new Timestamp(time);
-
-            observations.add(new Observation(o3, ts));
-
-            Object o4 = new Object(Type.CAR, "AABC22");
-            objects.put(o4.getIdentifier(), o4);
-
-            date = new Date();
-            time = date.getTime();
-            ts = new Timestamp(time);
-
-            observations.add(new Observation(o4, ts));
-        }
-        catch (Exception e) {}
-    }
+    public Silo() {}
 
     public Timestamp track(Type t, String s) throws NoObservationFound {
 
