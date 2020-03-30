99aae6307e94ca2cb856089e46396278339cf0eb
TiagoMAB <tiago.barroso1@hotmail.com>
2020-03-30 18:15:40 +0100

    Interface for testing server commands

--- silo-client/src/main/java/pt/tecnico/sauron/silo/client/SiloClientApp.java
+++ silo-client/src/main/java/pt/tecnico/sauron/silo/client/SiloClientApp.java
@@ -1,8 +1,21 @@
 package pt.tecnico.sauron.silo.client;
 
 
+import io.grpc.StatusRuntimeException;
+import pt.tecnico.sauron.silo.grpc.Observation;
+import pt.tecnico.sauron.silo.grpc.TraceRequest;
+import pt.tecnico.sauron.silo.grpc.TraceResponse;
+import pt.tecnico.sauron.silo.grpc.Type;
+
+import java.util.List;
+import java.util.Scanner;
+
 public class SiloClientApp {
-	
+	private static final String EXIT_CMD = "exit";
+	private static final String TRACE_CMD = "trace";
+	private static final String TRACK_CMD = "track";
+
+
 	public static void main(String[] args) {
 		System.out.println(SiloClientApp.class.getSimpleName());
 		
@@ -10,6 +23,50 @@ public class SiloClientApp {
 		System.out.printf("Received %d arguments%n", args.length);
 		for (int i = 0; i < args.length; i++) {
 			System.out.printf("arg[%d] = %s%n", i, args[i]);
+		}
+
+
+		// check arguments
+		if (args.length < 2) {
+			System.out.println("Argument(s) missing!");
+			System.out.printf("Usage: java %s host port%n", SiloClientApp.class.getName());
+			return;
+		}
+
+		final String host = args[0];
+		final int port = Integer.parseInt(args[1]);
+
+		try (SiloFrontend frontend = new SiloFrontend(host, port); Scanner scanner = new Scanner(System.in)) {
+			while (true) {
+				System.out.print("> Set the name (`exit` to quit)\n> ");
+				try {
+					String line = scanner.nextLine();
+
+					// exit
+					if (EXIT_CMD.equals(line))
+						break;
+
+					// get name
+					if (TRACE_CMD.equals(line)) {
+						TraceResponse getResponse = frontend.trace(TraceRequest.newBuilder().setType(Type.CAR).setIdentifier("AABB22").build());
+						List<Observation> obs = getResponse.getObservationsList();
+
+						for (Observation o: obs) {
+							System.out.println("Type: " + o.getType() + " | Identifier: " + o.getIdentifier() + " | Ts: " + o.getDate().toString());
+						}
+
+						continue;
+					}
+
+
+
+				} catch (StatusRuntimeException e) {
+					System.out.println(e.getStatus().getDescription());
+				}
+			}
+
+		} finally {
+			System.out.println("> Closing");
 		}
 	}
 	
