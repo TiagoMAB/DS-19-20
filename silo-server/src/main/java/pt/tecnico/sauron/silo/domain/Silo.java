6676e2e564d9190bf1a59ca56c36885d4a3ef9f3
Tiago Barroso <37189444+TiagoMAB@users.noreply.github.com>
2020-03-30 18:25:11 +0100

    Version 0.7.0
    * Fixed argument number of field observations from TraceResponse
    * Interface for testing server commands
    * Added trace() implementation
    * Fixed latitude and longitude, both are now doubles
    * Removed test code

--- silo-server/src/main/java/pt/tecnico/sauron/silo/domain/Silo.java
+++ silo-server/src/main/java/pt/tecnico/sauron/silo/domain/Silo.java
@@ -5,6 +5,7 @@ import pt.tecnico.sauron.silo.domain.exceptions.*;
 
 import java.sql.Timestamp;
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 public class Silo {
 
@@ -12,7 +13,7 @@ public class Silo {
     private TreeSet observations = new TreeSet<Observation>();
     private HashSet cameras = new HashSet<Camera>();
 
-    public Silo() { }
+    public Silo() {}
 
     public Timestamp track(Type t, String s) throws NoObservationFound {
 
@@ -30,4 +31,26 @@ public class Silo {
         throw new NoObservationFound(s);
     }
 
+    public List<Observation> trace(Type t, String s) throws NoObservationFound {
+
+        List<Observation> list = new ArrayList<Observation>();
+
+        Iterator it = observations.descendingIterator();
+
+        while (it.hasNext()) {
+            Observation observation = (Observation) it.next();
+
+            Object object = observation.getObject();
+
+            if (object.getIdentifier().equals(s) && object.getType() == t) {
+                list.add(observation);
+            }
+        }
+        if (list.isEmpty()) {
+            throw new NoObservationFound(s);
+        }
+        else {
+            return list;
+        }
+    }
 }
