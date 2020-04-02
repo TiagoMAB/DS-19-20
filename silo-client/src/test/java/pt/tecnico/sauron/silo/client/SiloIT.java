5bf95cc98788f51b0e7feafbda6cf34d3a1b6f5d
TiagoMAB <tiago.barroso1@hotmail.com>
2020-04-02 09:56:40 +0100

    Changed type in protobuf to accomodate 'unknown'

--- silo-client/src/test/java/pt/tecnico/sauron/silo/client/SiloIT.java
+++ silo-client/src/test/java/pt/tecnico/sauron/silo/client/SiloIT.java
@@ -1,6 +1,17 @@
 package pt.tecnico.sauron.silo.client;
 
+import io.grpc.StatusRuntimeException;
 import org.junit.jupiter.api.*;
+import pt.tecnico.sauron.silo.grpc.TrackRequest;
+
+
+import static io.grpc.Status.Code.INVALID_ARGUMENT;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class SiloIT extends BaseIT {
 	
@@ -35,8 +46,10 @@ public class SiloIT extends BaseIT {
 	
 	@Test
 	public void test() {
-		
-		
+
+		assertEquals(INVALID_ARGUMENT,
+				assertThrows(StatusRuntimeException.class, () -> SiloFrontend.track(TrackRequest.newBuilder().getDefaultInstanceForType())).getStatus()
+						.getCode());
 	}
 
 }
--- silo-server/src/main/java/pt/tecnico/sauron/silo/domain/exceptions/DuplicateCameraName.java
+++
@@ -1,9 +0,0 @@
-package pt.tecnico.sauron.silo.domain.exceptions;
-
-public class DuplicateCameraName extends Exception {
-
-    public DuplicateCameraName(String s) {
-        super(s);
-    }
-
-}
